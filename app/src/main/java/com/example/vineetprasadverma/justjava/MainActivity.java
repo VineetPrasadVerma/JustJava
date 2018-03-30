c9607c173adb91ab328ef335dec0e37d714f0233
Vineet Prasad Verma <vineetpd1996@gmail.com>
2018-03-30 11:16:22 +0530

    Added more functionalities to app like adding a checkbox and a edit text view
    Included option to choose to select if user want chocolate or whipped cream in cofffee and also added one edit text view to add the name of the customer and also updated the quantity variables so that now user cant order less tha 1 or more than 100 cups of coffee

--- app/src/main/java/com/example/vineetprasadverma/justjava/MainActivity.java
+++ app/src/main/java/com/example/vineetprasadverma/justjava/MainActivity.java
@@ -6,14 +6,17 @@ package com.example.vineetprasadverma.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity extends AppCompatActivity {
 
-    int quantity = 0;
+    int quantity = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,33 +28,60 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price =  calculatePrice();
-        displayMessage(createOrderSummary(price));
+        EditText nameOfCustomer = (EditText) findViewById(R.id.name_view);
+        String name = nameOfCustomer.getText().toString();
+        CheckBox whippedCreamCheckBox= (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        Boolean hasWhippedCream = whippedCreamCheckBox.isChecked();
+        CheckBox chocolateCheckBox= (CheckBox) findViewById(R.id.chocolate_checkbox);
+        Boolean hasChocolate = chocolateCheckBox.isChecked();
+        int price =  calculatePrice(hasWhippedCream, hasChocolate);
+        displayMessage(createOrderSummary(name,price,hasWhippedCream,hasChocolate));
     }
 
     /**
      * Calculates the price of the order.
+     * @param addChocolate whether user wants chocolate or not
+     * @param addWhippedCream whether user wants whippedcream or not
      */
-    private int calculatePrice() {
-        return quantity * 5;
+    private int calculatePrice(boolean addWhippedCream, boolean addChocolate) {
+        int basePrice = 10;
+        if(addWhippedCream){
+            basePrice = basePrice + 5 ;
+        }
+        if(addChocolate){
+            basePrice = basePrice + 7;
+        }
+        return quantity * basePrice;
     }
 
     /**
      * create summary order
-     * @price of the order
+     * @param price of the order
+     * @param hasWhippedCream is whether or not the user wants topping or not
+     * @param hasChocolate is check whether user wants chocolate or not
      * @return text summary
      */
-    private String createOrderSummary(int price){
-        return  "Name : Vineet Verma\nQuantity : "+quantity+"\nTotal : ₹"+price+"\nThank you!";
+    private String createOrderSummary(String name, int price, boolean hasWhippedCream, boolean hasChocolate){
+
+        return  name+"\nAdd Whipped Cream ? "+hasWhippedCream+"\nAdd Chocolate ? "+hasChocolate+"\nQuantity : "+quantity+"\nTotal : ₹"+price+"\nThank you!";
     }
 
     public void increment(View view) {
-        quantity = quantity+1;
+
+        if(quantity < 100) {
+            quantity = quantity + 1;
+        }else{
+            Toast.makeText(this, "You can't order more than 100 Coffee",Toast.LENGTH_LONG).show();
+        }
         displayQuantity(quantity);
     }
 
     public void decrement(View view) {
-        quantity = quantity-1;
+        if(quantity > 1) {
+            quantity = quantity - 1;
+        }else{
+            Toast.makeText(this, "You can't order less than 1 Coffee",Toast.LENGTH_LONG).show();
+        }
         displayQuantity(quantity);
     }
 
